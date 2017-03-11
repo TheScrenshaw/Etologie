8a2297cf45a79c95cd0fb37e964704c0e015f374
TheScrenshaw <screnshaw99@gmail.com>
2017-03-11 11:26:35 +0100

    Too many changes too put in words

---
+++ cs/vyuka/education_js/dropdown-btn.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#btn-programmes-prf").click(function(){
+        $(this).next().toggleClass("down");
+    });
+    $("#btn-programmes-fhs-bc").click(function(){
+        $(this).next().toggleClass("down");
+    });
+    $("#btn-programmes-fhs-mg").click(function(){
+        $(this).next().toggleClass("down");
+    });
+});
